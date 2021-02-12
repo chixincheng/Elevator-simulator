cabc1f0536e2634ef13d911f930178b19f5fa65f
chixincheng <chixincheng@gmail.com>
2021-02-12 14:12:48 -0500

    Update Request.java

--- Request.java
+++ Request.java
@@ -4,9 +4,7 @@
  * timeEntered, all three member variables are ints.
  * 
  * @author
- *      XinCheng Chi, SBU ID#：111919385,Recition R30
- * Assignment:
- *      Homework #3 for CSE 214, Summer 2020
+ *      XinCheng Chi
  * Date:
  *      July 23,2020
  */
