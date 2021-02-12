1db973bbc64e10e4eb8fdff17c4c322fd611c730
chixincheng <chixincheng@gmail.com>
2021-02-12 14:11:53 -0500

    Update ExpressElevator.java

--- ExpressElevator.java
+++ ExpressElevator.java
@@ -7,9 +7,7 @@
  * it contains a boolean to show direction of the elevator
  * 
  * @author
- *      XinCheng Chi, SBU ID#：111919385,Recition R30
- * Assignment:
- *      Homework #3 for CSE 214, Summer 2020
+ *      XinCheng Chi
  * Date:
  *      July 23,2020
  */
